eddc443b80343908b8a013cd181dab46d949e07b
BarbaraTieko <barbara_tieko@hotmail.com>
2023-05-12 15:49:05 -0300

    Adicionando Procfile e atualizando requirements.txt

--- getit/settings.py
+++ getit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@77s)-69z83z6+-&%i+oad4t&28#ydwjwme57!z7lcl%@0*i9m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
